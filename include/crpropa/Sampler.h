712f596ca733d772bdada2f224b4dba5aade310e
Rafael <rafaelab@users.noreply.github.com>
2022-10-15 14:26:55 +0200

    add more samplers based on functions

--- include/crpropa/Sampler.h
+++ include/crpropa/Sampler.h
@@ -23,7 +23,7 @@ namespace crpropa {
  */
 class Sampler: public Referenced {
 	public:
-		virtual double computeWeight(int id, double energy = 0, double energyFraction = 0) const = 0;
+		virtual double computeWeight(int id, double energy = 0, double energyFraction = 0, int counter = 0) const = 0;
 };
 
 
@@ -47,9 +47,34 @@ class SamplerUniform: public Sampler {
 		void setParticleId(int particleId);
 		double getSampling() const;
 		int getParticleId() const;
-		double computeWeight(int id, double energy = 0, double energyFraction = 0) const;
+		double computeWeight(int id, double energy = 0, double energyFraction = 0, int counter = 0) const;
 };
 
+
+/**
+ @class SamplerEnergyFractionPowerLaw
+ @brief Throw away a fraction of the particles of a given type according to a power law associated with the energy fraction it takes.
+ The sampling parameter controls whether all (sampling=1) or no (sampling=0) particles are accepted.
+ This is essentially the implementation of the thinning of the EM interactions for index=1.
+ In this case, the thinning is defined as: thinning = 1 - sampling.
+
+ */
+class SamplerEnergyFractionPowerLaw: public Sampler {
+	private:
+		int particleId;
+		double sampling;
+		double index;
+
+	public:
+		SamplerEnergyFractionPowerLaw(double index, int particleId, double sampling);
+		void setSampling(double sampling);
+		void setParticleId(int particleId);
+		void setIndex(double index);
+		double getSampling() const;
+		int getParticleId() const;
+		double getIndex() const;
+		double computeWeight(int id, double energy = 0, double energyFraction = 0, int counter = 0) const;
+};
 
 /**
  @class SamplerEnergyFraction
@@ -58,19 +83,12 @@ class SamplerUniform: public Sampler {
  This is essentially the implementation of the thinning of the EM interactions. 
  In this case, the thinning is defined as: thinning = 1 - sampling
  */
-class SamplerEnergyFraction: public Sampler {
-	private:
-		int particleId;
-		double sampling;
-
+class SamplerEnergyFraction: public SamplerEnergyFractionPowerLaw {
 	public:
-		SamplerEnergyFraction(int particleId, double sampling);
-		void setSampling(double sampling);
-		void setParticleId(int particleId);
-		double getSampling() const;
-		int getParticleId() const;
-		double computeWeight(int id, double energy = 0, double energyFraction = 0) const;
+		SamplerEnergyFraction(int particleId, double sampling) : SamplerEnergyFractionPowerLaw(1, particleId, sampling) {}
 };
+
+
 
 /**
  @class SamplerNull
@@ -79,8 +97,9 @@ class SamplerEnergyFraction: public Sampler {
 class SamplerNull : public Sampler {
 	public:
 		SamplerNull();
-		double computeWeight(int id, double energy = 0, double energyFraction = 0) const;
+		double computeWeight(int id, double energy = 0, double energyFraction = 0, int counter = 0) const;
 };
+
 
 
 /**
@@ -89,15 +108,15 @@ class SamplerNull : public Sampler {
  */
 class SamplerList : public Sampler {
 	protected:
-		std::vector< ref_ptr<Sampler> > samplers;
+		std::vector<ref_ptr<Sampler>> samplers;
 	public:
 		SamplerList();
 		SamplerList(std::vector<ref_ptr<Sampler>> samplers);
 		void add(Sampler *sampler);
-		inline void add(ref_ptr<Sampler> sampler) {
-			add(sampler.get());
-		}
-		double computeWeight(int id, double energy = 0, double energyFraction = 0) const;
+		// inline void add(ref_ptr<Sampler> sampler) {
+		// 	add(sampler.get());
+		// }
+		double computeWeight(int id, double energy = 0, double energyFraction = 0, int counter = 0) const;
 };
 
 } // namespace crpropa
--- src/Sampler.cpp
+++ src/Sampler.cpp
@@ -27,13 +27,13 @@ int SamplerUniform::getParticleId() const {
 	return particleId;
 }
 
-double SamplerUniform::computeWeight(int id, double E, double f) const {
+double SamplerUniform::computeWeight(int id, double E, double f, int counter) const {
 	if (id != particleId)
 		return 0;
 
-	if (sampling == 1.) {
+	if (sampling >= 1.) {
 		return 1;
-	} else if (sampling == 0) {
+	} else if (sampling <= 0) {
 		return 0;
 	} else {
 		Random &random = Random::instance();
@@ -47,49 +47,64 @@ double SamplerUniform::computeWeight(int id, double E, double f) const {
 
 /****************************************************************************/
 
-SamplerEnergyFraction::SamplerEnergyFraction(int pId, double s) {
+SamplerEnergyFractionPowerLaw::SamplerEnergyFractionPowerLaw(double idx, int pId, double s) {
 	setSampling(s);
 	setParticleId(pId);
+	setIndex(idx);
 }
 
-void SamplerEnergyFraction::setSampling(double s) {
+void SamplerEnergyFractionPowerLaw::setSampling(double s) {
 	sampling = s;
 }
 
-void SamplerEnergyFraction::setParticleId(int pId) {
+void SamplerEnergyFractionPowerLaw::setParticleId(int pId) {
 	particleId = pId;
 }
 
-double SamplerEnergyFraction::getSampling() const {
+void SamplerEnergyFractionPowerLaw::setIndex(double idx) {
+	index = idx;
+}
+
+double SamplerEnergyFractionPowerLaw::getSampling() const {
 	return sampling;
 }
 
-int SamplerEnergyFraction::getParticleId() const {
+int SamplerEnergyFractionPowerLaw::getParticleId() const {
 	return particleId;
 }
 
-double  SamplerEnergyFraction::computeWeight(int id, double E, double f) const {
+double SamplerEnergyFractionPowerLaw::getIndex() const {
+	return index;
+}
+
+double  SamplerEnergyFractionPowerLaw::computeWeight(int id, double E, double f, int counter) const {
 	if (id != particleId)
 		return 0;
 
-	if (sampling == 1) {
+	if (sampling >= 1) {
 		return 1;
-	} else if (sampling == 0) {
+	} else if (sampling < 0) {
 		return 0;
 	} else {
 		Random &random = Random::instance();
-		if (random.rand() < pow(f, 1 - sampling)) // accept and return weight
-			return 1 / pow(f, 1 - sampling);
+		double r = 0;
+		if (index < 0)
+			r = pow(1 - f, (1 - sampling) * abs(index));
+		else
+			r = pow(f, (1 - sampling) * index);
+		if (random.rand() < r) // accept and return weight
+			return 1. / r;
 		else // reject
 			return 0;
 	}
 }
 
+
 /****************************************************************************/
 SamplerNull::SamplerNull() {
 }
 
-double SamplerNull::computeWeight(int id, double E, double f) const {
+double SamplerNull::computeWeight(int id, double E, double f, int counter) const {
 	return 1.;
 }
 
@@ -108,10 +123,17 @@ void SamplerList::add(Sampler *sampler) {
 	samplers.push_back(sampler);
 }
 
-double SamplerList::computeWeight(int id, double E, double f) const {
-	double w = 1.;
+double SamplerList::computeWeight(int id, double E, double f, int counter) const {
+	double w = 0;
+	int k = 0;
 	for (size_t i = 0; i < samplers.size(); i++) {
-		w *= samplers[i]->computeWeight(id, E, f);
+		double w0 = samplers[i]->computeWeight(id, E, f, counter);
+		if (w0 > 0) {
+			if (k == 0)
+				w = 1;
+			w *= w0;
+			k++;
+		}
 	}
 	
 	return w;
