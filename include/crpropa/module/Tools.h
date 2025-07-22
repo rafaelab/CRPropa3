2cd457223c000860951d27a19bd63cdd2c25ff9b
Rafael <rafaelab@users.noreply.github.com>
2025-07-23 01:02:34 +0200

    minor style changes

--- include/crpropa/module/Tools.h
+++ include/crpropa/module/Tools.h
@@ -1,11 +1,13 @@
 #ifndef CRPROPA_MODULETOOLS_H
 #define CRPROPA_MODULETOOLS_H
 
+#include <vector>
+#include <set>
+
 #include "crpropa/Module.h"
 #include "crpropa/EmissionMap.h"
 
-#include <vector>
-#include <set>
+
 
 namespace crpropa {
 /**
@@ -14,58 +16,59 @@ namespace crpropa {
  */
 
 /**
- @class PerformanceModule
- @brief Module to monitor the simulation performance
-
- Add modules under investigation to this module instead of the ModuleList.
+ * @class PerformanceModule
+ * @brief Module to monitor the simulation performance.
+ *
+ * Add modules under investigation to this module instead of the ModuleList.
  */
 class PerformanceModule: public Module {
-private:
-	struct _module_info {
-		double time;
-		ref_ptr<Module> module;
-	};
+	private:
+		struct _module_info {
+			double time;
+			ref_ptr<Module> module;
+		};
+		mutable std::vector<_module_info> modules;
+		mutable size_t calls;
 
-	mutable std::vector<_module_info> modules;
-	mutable size_t calls;
-
-public:
-	~PerformanceModule();
-	void add(Module* module);
-	void process(Candidate* candidate) const;
-	std::string getDescription() const;
+	public:
+		~PerformanceModule();
+		void add(Module* module);
+		void process(Candidate* candidate) const;
+		std::string getDescription() const;
 };
 
 /**
-  @class ParticleFilter
-  @brief Reject Particles not listed in filter.
-*/
+ * @class ParticleFilter
+ * @brief Reject Particles not listed in filter.
+ */
 class ParticleFilter: public AbstractCondition {
-	std::set<int> ids;
+	protected:
+		std::set<int> ids;
 
-public:
-	ParticleFilter();
-	ParticleFilter(const std::set<int> &ids);
-	void addId(int id);
-	void removeId(int remove);
-	std::set<int> &getIds();
-
-	void process(Candidate* candidate) const;
-	std::string getDescription() const;
+	public:
+		ParticleFilter();
+		ParticleFilter(const std::set<int>& ids);
+		void addId(int id);
+		void removeId(int remove);
+		std::set<int>& getIds();
+		void process(Candidate* candidate) const;
+		std::string getDescription() const;
 };
 
 
 /**
-  @class EmissionMapFiller
-  @brief Fill EmissionMap with source particle state
-*/
+ * @class EmissionMapFiller
+ * @brief Fill EmissionMap with source particle state
+ */
 class EmissionMapFiller: public Module {
-	ref_ptr<EmissionMap> emissionMap;
-public:
-	EmissionMapFiller(EmissionMap *emissionMap);
-	void setEmissionMap(EmissionMap *emissionMap);
-	void process(Candidate* candidate) const;
-	std::string getDescription() const;
+	protected:
+		ref_ptr<EmissionMap> emissionMap;
+
+	public:
+		EmissionMapFiller(EmissionMap* emissionMap);
+		void setEmissionMap(EmissionMap* emissionMap);
+		void process(Candidate* candidate) const;
+		std::string getDescription() const;
 };
 
 /** @}*/
