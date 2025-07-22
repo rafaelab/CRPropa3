001fb2ac3e007a209ebf8148b85bc675bf4b2a10
Rafael <rafaelab@users.noreply.github.com>
2025-07-22 17:17:10 +0200

    improve style

--- include/crpropa/module/TextOutput.h
+++ include/crpropa/module/TextOutput.h
@@ -1,10 +1,24 @@
 #ifndef CRPROPA_TEXTOUTPUT_H
 #define CRPROPA_TEXTOUTPUT_H
 
+#include <cstdio>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+#ifdef CRPROPA_HAVE_ZLIB
+	#include <izstream.hpp>
+	#include <ozstream.hpp>
+#endif
+
+#include "crpropa/base64.h"
+#include "crpropa/Random.h"
+#include "crpropa/Units.h"
+#include "crpropa/Version.h"
 #include "crpropa/module/Output.h"
 #include "crpropa/module/ParticleCollector.h"
+#include "kiss/string.h"
 
-#include <fstream>
 
 namespace crpropa {
 /**
@@ -13,66 +27,79 @@ namespace crpropa {
  */
 
 /**
- @class TextOutput
- @brief Configurable plain text output for particle information.
- This type of output can also be used to generate a .tar.gz file if
- the library zlib is available. For details see:
- 	http://zlib.net/
+ * @class TextOutput
+ * @brief Configurable plain text output for particle information.
+ * This type of output can also be used to generate a .tar.gz file if the library zlib is available. 
+ * For details see: http://zlib.net/
  */
 class TextOutput: public Output {
-protected:
-	std::ostream *out;
-	std::ofstream outfile;
-	std::string filename;
-	bool storeRandomSeeds;
-	
-	void printHeader() const;
+	protected:
+		std::ostream* out;
+		std::ofstream outfile;
+		std::string filename;
+		bool storeRandomSeeds;
+		
+		void printHeader() const;
 
-public:
-	/** Default constructor
-	 */
-	TextOutput();
-	/** Constructor
-	 @param outputType	type of output: Trajectory1D, Trajectory3D, Event1D, Event3D, Everything
-	 */
-	TextOutput(OutputType outputType);
-	/** Constructor
-	 @param out			output stream
-	 */
-	TextOutput(std::ostream &out);
-	/** Constructor
-	 @param out			output stream
-	 @param outputType	type of output: Trajectory1D, Trajectory3D, Event1D, Event3D, Everything
-	 */
-	TextOutput(std::ostream &out, OutputType outputType);
-	/** Constructor with the default OutputType (everything).
-	 @param filename	string containing name of output text file
-	 */
-	TextOutput(const std::string &filename);
-	/** Constructor
-	 @param filename	string containing name of output text file
-	 @param outputType	type of output: Trajectory1D, Trajectory3D, Event1D, Event3D, Everything
-	 */
-	TextOutput(const std::string &filename, OutputType outputType);
-	/** Destructor
-	 */
-	~TextOutput();
-	/** Whether to store the random seeds used in the simulation.
-	 This enables reproducibility of each realisation of the simulation.
-	 */
-	void enableRandomSeeds() {storeRandomSeeds = true;};
-	void close();
-	void gzip();
-	void process(Candidate *candidate) const;
-	/** Loads a file to a particle collector.
-	 This is useful for analysis involving, e.g., magnetic lenses.
-	 @param filename	string containing the name of the file to be loaded
-	 @param collector	object of type ParticleCollector that will store the information
-	 */
-	static void load(const std::string &filename, ParticleCollector *collector);
-	std::string getDescription() const;
+	public:
+		/** Default constructor
+		 */
+		TextOutput();
 
-	void dumpIndexList(std::vector<int> indicies);
+		/** Constructor
+		 * @param outputType	types of output: Trajectory1D, Trajectory3D, Event1D, Event3D, Everything
+		 */
+		TextOutput(OutputType outputType);
+
+		/** Constructor
+		 * @param filename	string containing name of output text file
+		 */
+		/** Constructor
+		 * @param out			output stream
+		 */
+		TextOutput(std::ostream& out);
+
+		/** Constructor
+		 * @param out			output stream
+		 * @param outputType	type of output: Trajectory1D, Trajectory3D, Event1D, Event3D, Everything
+		 */
+		TextOutput(std::ostream& out, OutputType outputType);
+		
+		/** Constructor with the default OutputType (everything).
+		 * @param filename	string containing name of output text file
+		 */
+		TextOutput(const std::string& filename);
+
+		/** Constructor
+		 * @param filename	string containing name of output text file
+		 * @param outputType	type of output: Trajectory1D, Trajectory3D, Event1D, Event3D, Everything
+		 */
+		TextOutput(const std::string& filename, OutputType outputType);
+
+		/** Destructor
+		 */
+		~TextOutput();
+
+		/** Whether to store the random seeds used in the simulation.
+		 * This enables reproducibility of each realisation of the simulation.
+		 */
+		void enableRandomSeeds() {
+			storeRandomSeeds = true;
+		};
+		void close();
+		void gzip();
+		void process(Candidate *candidate) const;
+
+		/** Loads a file to a particle collector.
+		 * This is useful for analysis involving, e.g., magnetic lenses.
+		 * @param filename	string containing the name of the file to be loaded
+		 * @param collector	object of type ParticleCollector that will store the information
+		 */
+		static void load(const std::string& filename, ParticleCollector* collector);
+
+		std::string getDescription() const;
+
+		void dumpIndexList(std::vector<int> indicies);
 };
 /** @}*/
 
--- src/module/TextOutput.cpp
+++ src/module/TextOutput.cpp
@@ -1,21 +1,5 @@
 #include "crpropa/module/TextOutput.h"
-#include "crpropa/module/ParticleCollector.h"
-#include "crpropa/Units.h"
-#include "crpropa/Version.h"
-#include "crpropa/Random.h"
-#include "crpropa/base64.h"
 
-#include "kiss/string.h"
-
-#include <sstream>
-#include <cstdio>
-#include <stdexcept>
-#include <iostream>
-
-#ifdef CRPROPA_HAVE_ZLIB
-#include <izstream.hpp>
-#include <ozstream.hpp>
-#endif
 
 namespace crpropa {
 
@@ -25,27 +9,24 @@ TextOutput::TextOutput() : Output(), out(&std::cout), storeRandomSeeds(false) {
 TextOutput::TextOutput(OutputType outputtype) : Output(outputtype), out(&std::cout), storeRandomSeeds(false) {
 }
 
-TextOutput::TextOutput(std::ostream &out) : Output(), out(&out), storeRandomSeeds(false) {
+TextOutput::TextOutput(std::ostream& out) : Output(), out(&out), storeRandomSeeds(false) {
 }
 
-TextOutput::TextOutput(std::ostream &out,
-		OutputType outputtype) : Output(outputtype), out(&out), storeRandomSeeds(false) {
+TextOutput::TextOutput(std::ostream& out, OutputType outputtype) : 
+	Output(outputtype), out(&out), storeRandomSeeds(false) {
 }
 
-TextOutput::TextOutput(const std::string &filename) :  Output(), outfile(filename.c_str(),
-				std::ios::binary), out(&outfile),  filename(
-				filename), storeRandomSeeds(false) {
-	if (!outfile.is_open())
+TextOutput::TextOutput(const std::string& filename) :  
+Output(), outfile(filename.c_str(), std::ios::binary), out(&outfile),  filename(filename), storeRandomSeeds(false) {
+	if (! outfile.is_open())
 		throw std::runtime_error(std::string("Cannot create file: ") + filename);
 	if (kiss::ends_with(filename, ".gz"))
 		gzip();
 }
 
-TextOutput::TextOutput(const std::string &filename,
-				OutputType outputtype) : Output(outputtype), outfile(filename.c_str(),
-				std::ios::binary), out(&outfile), filename(
-				filename), storeRandomSeeds(false) {
-	if (!outfile.is_open())
+TextOutput::TextOutput(const std::string& filename, OutputType outputtype) : 
+	Output(outputtype), outfile(filename.c_str(), std::ios::binary), out(&outfile), filename(filename), storeRandomSeeds(false) {
+	if (! outfile.is_open())
 		throw std::runtime_error(std::string("Cannot create file: ") + filename);
 	if (kiss::ends_with(filename, ".gz"))
 		gzip();
@@ -63,11 +44,11 @@ void TextOutput::printHeader() const {
 		*out << "\tID";
 	if (fields.test(CurrentEnergyColumn))
 		*out << "\tE";
-	if (fields.test(CurrentPositionColumn) && oneDimensional)
+	if (fields.test(CurrentPositionColumn) and oneDimensional)
 		*out << "\tX";
-	if (fields.test(CurrentPositionColumn) && not oneDimensional)
+	if (fields.test(CurrentPositionColumn) and not oneDimensional)
 		*out << "\tX\tY\tZ";
-	if (fields.test(CurrentDirectionColumn) && not oneDimensional)
+	if (fields.test(CurrentDirectionColumn) and not oneDimensional)
 		*out << "\tPx\tPy\tPz";
 	if (fields.test(SerialNumberColumn))
 		*out << "\tSN0";
@@ -75,11 +56,11 @@ void TextOutput::printHeader() const {
 		*out << "\tID0";
 	if (fields.test(SourceEnergyColumn))
 		*out << "\tE0";
-	if (fields.test(SourcePositionColumn) && oneDimensional) 
+	if (fields.test(SourcePositionColumn) and oneDimensional)
 		*out << "\tX0";
-	if (fields.test(SourcePositionColumn) && not oneDimensional)
+	if (fields.test(SourcePositionColumn) and not oneDimensional)
 		*out << "\tX0\tY0\tZ0";
-	if (fields.test(SourceDirectionColumn) && not oneDimensional)
+	if (fields.test(SourceDirectionColumn) and not oneDimensional)
 		*out << "\tP0x\tP0y\tP0z";
 	if (fields.test(SerialNumberColumn))
 		*out << "\tSN1";
@@ -87,42 +68,34 @@ void TextOutput::printHeader() const {
 		*out << "\tID1";
 	if (fields.test(CreatedEnergyColumn))
 		*out << "\tE1";
-	if (fields.test(CreatedPositionColumn) && oneDimensional)
+	if (fields.test(CreatedPositionColumn) and oneDimensional)
 		*out << "\tX1";
-	if (fields.test(CreatedPositionColumn) && not oneDimensional)
+	if (fields.test(CreatedPositionColumn) and not oneDimensional)
 		*out << "\tX1\tY1\tZ1";
-	if (fields.test(CreatedDirectionColumn) && not oneDimensional)
+	if (fields.test(CreatedDirectionColumn) and not oneDimensional)
 		*out << "\tP1x\tP1y\tP1z";
 	if (fields.test(WeightColumn))
 		*out << "\tW";
 	if (fields.test(CandidateTagColumn))
 		*out << "\ttag";
-	for(std::vector<Property>::const_iterator iter = properties.begin();
-			iter != properties.end(); ++iter)
-	{
+	for (std::vector<Property>::const_iterator iter = properties.begin(); iter != properties.end(); ++iter) {
 		*out << "\t" << (*iter).name;
 	}
 
 	*out << "\n#\n";
 	if (fields.test(TrajectoryLengthColumn))
-		*out << "# D             Trajectory length [" << lengthScale / Mpc
-				<< " Mpc]\n";
+		*out << "# D             Trajectory length [" << lengthScale / Mpc << " Mpc]\n";
 	if (fields.test(RedshiftColumn))
 		*out << "# z             Redshift\n";
 	if (fields.test(SerialNumberColumn))
 		*out << "# SN/SN0/SN1    Serial number. Unique (within this run) id of the particle.\n";
-	if (fields.test(CurrentIdColumn) || fields.test(CreatedIdColumn)
-			|| fields.test(SourceIdColumn))
+	if (fields.test(CurrentIdColumn) or fields.test(CreatedIdColumn) or fields.test(SourceIdColumn))
 		*out << "# ID/ID0/ID1    Particle type (PDG MC numbering scheme)\n";
-	if (fields.test(CurrentEnergyColumn) || fields.test(CreatedEnergyColumn)
-			|| fields.test(SourceEnergyColumn))
+	if (fields.test(CurrentEnergyColumn) or fields.test(CreatedEnergyColumn) or fields.test(SourceEnergyColumn))
 		*out << "# E/E0/E1       Energy [" << energyScale / EeV << " EeV]\n";
-	if (fields.test(CurrentPositionColumn) || fields.test(CreatedPositionColumn)
-			|| fields.test(SourcePositionColumn))
+	if (fields.test(CurrentPositionColumn) or fields.test(CreatedPositionColumn) or fields.test(SourcePositionColumn))
 		*out << "# X/X0/X1...    Position [" << lengthScale / Mpc << " Mpc]\n";
-	if (fields.test(CurrentDirectionColumn)
-			|| fields.test(CreatedDirectionColumn)
-			|| fields.test(SourceDirectionColumn))
+	if (fields.test(CurrentDirectionColumn) or fields.test(CreatedDirectionColumn) or fields.test(SourceDirectionColumn))
 		*out << "# Px/P0x/P1x... Heading (unit vector of momentum)\n";
 	if (fields.test(WeightColumn))
 		*out << "# W             Weights" << " \n";
@@ -133,22 +106,18 @@ void TextOutput::printHeader() const {
 			<< "#\tND  \tNuclearDecay\n" << "#\tPD  \tPhotoDisintegration\n" << "#\tPPP  \tPhotoPionProduction\n" << "#\tSYN \tSynchrotronRadiation\n"
 			<< "#\tPRIM/SEC\t primary / secondary particle\n";
 	}
-	for(std::vector<Property>::const_iterator iter = properties.begin();
-			iter != properties.end(); ++iter)
-	{
-			*out << "# " << (*iter).name << " " << (*iter).comment << "\n";
+	for(std::vector<Property>::const_iterator iter = properties.begin(); iter != properties.end(); ++iter) {
+		*out << "# " << (*iter).name << " " << (*iter).comment << "\n";
 	}
 
 	*out << "# no index = current, 0 = at source, 1 = at point of creation\n#\n";
 	*out << "# CRPropa version: " << g_GIT_DESC << "\n#\n";
 
-	if (storeRandomSeeds)
-	{
+	if (storeRandomSeeds) {
 		*out << "# Random seeds:\n";
-		std::vector< std::vector<uint32_t> > seeds = Random::getSeedThreads();
+		std::vector<std::vector<uint32_t>> seeds = Random::getSeedThreads();
 
-		for (size_t i =0; i < seeds.size(); i++)
-		{
+		for (size_t i = 0; i < seeds.size(); i++) {
 			std::string encoded_data = Base64::encode((unsigned char*) &seeds[i][0], sizeof(seeds[i][0]) * seeds[i].size() / sizeof(unsigned char));
 			*out << "#   Thread " << i << ": ";
 			*out << encoded_data;
@@ -157,8 +126,8 @@ void TextOutput::printHeader() const {
 	}
 }
 
-void TextOutput::process(Candidate *c) const {
-	if (fields.none() && properties.empty())
+void TextOutput::process(Candidate* c) const {
+	if (fields.none() and properties.empty())
 		return;
 
 	char buffer[1024];
@@ -167,35 +136,27 @@ void TextOutput::process(Candidate *c) const {
 	std::locale old_locale = std::locale::global(std::locale::classic());
 
 	if (fields.test(TrajectoryLengthColumn))
-		p += std::sprintf(buffer + p, "%8.5E\t",
-				c->getTrajectoryLength() / lengthScale);
-
+		p += std::sprintf(buffer + p, "%8.5E\t", c->getTrajectoryLength() / lengthScale);
 	if (fields.test(RedshiftColumn))
 		p += std::sprintf(buffer + p, "%1.5E\t", c->getRedshift());
-
 	if (fields.test(SerialNumberColumn))
-		p += std::sprintf(buffer + p, "%10lu\t",
-				c->getSerialNumber());
+		p += std::sprintf(buffer + p, "%10lu\t", c->getSerialNumber());
 	if (fields.test(CurrentIdColumn))
 		p += std::sprintf(buffer + p, "%10i\t", c->current.getId());
 	if (fields.test(CurrentEnergyColumn))
-		p += std::sprintf(buffer + p, "%8.5E\t",
-				c->current.getEnergy() / energyScale);
+		p += std::sprintf(buffer + p, "%8.5E\t", c->current.getEnergy() / energyScale);
 	if (fields.test(CurrentPositionColumn)) {
 		if (oneDimensional) {
-			p += std::sprintf(buffer + p, "%8.5E\t",
-					c->current.getPosition().x / lengthScale);
+			p += std::sprintf(buffer + p, "%8.5E\t", c->current.getPosition().x / lengthScale);
 		} else {
 			const Vector3d pos = c->current.getPosition() / lengthScale;
-			p += std::sprintf(buffer + p, "%8.5E\t%8.5E\t%8.5E\t", pos.x, pos.y,
-					pos.z);
+			p += std::sprintf(buffer + p, "%8.5E\t%8.5E\t%8.5E\t", pos.x, pos.y, pos.z);
 		}
 	}
 	if (fields.test(CurrentDirectionColumn)) {
 		if (not oneDimensional) {
 			const Vector3d pos = c->current.getDirection();
-			p += std::sprintf(buffer + p, "%8.5E\t%8.5E\t%8.5E\t", pos.x, pos.y,
-					pos.z);
+			p += std::sprintf(buffer + p, "%8.5E\t%8.5E\t%8.5E\t", pos.x, pos.y, pos.z);
 		}
 	}
 
@@ -204,50 +165,41 @@ void TextOutput::process(Candidate *c) const {
 	if (fields.test(SourceIdColumn))
 		p += std::sprintf(buffer + p, "%10i\t", c->source.getId());
 	if (fields.test(SourceEnergyColumn))
-		p += std::sprintf(buffer + p, "%8.5E\t",
-				c->source.getEnergy() / energyScale);
+		p += std::sprintf(buffer + p, "%8.5E\t", c->source.getEnergy() / energyScale);
 	if (fields.test(SourcePositionColumn)) {
 		if (oneDimensional) {
-			p += std::sprintf(buffer + p, "%8.5E\t",
-					c->source.getPosition().x / lengthScale);
+			p += std::sprintf(buffer + p, "%8.5E\t", c->source.getPosition().x / lengthScale);
 		} else {
 			const Vector3d pos = c->source.getPosition() / lengthScale;
-			p += std::sprintf(buffer + p, "%8.5E\t%8.5E\t%8.5E\t", pos.x, pos.y,
-					pos.z);
+			p += std::sprintf(buffer + p, "%8.5E\t%8.5E\t%8.5E\t", pos.x, pos.y, pos.z);
 		}
 	}
 	if (fields.test(SourceDirectionColumn)) {
 		if (not oneDimensional) {
 			const Vector3d pos = c->source.getDirection();
-			p += std::sprintf(buffer + p, "%8.5E\t%8.5E\t%8.5E\t", pos.x, pos.y,
-					pos.z);
+			p += std::sprintf(buffer + p, "%8.5E\t%8.5E\t%8.5E\t", pos.x, pos.y, pos.z);
 		}
 
 	}
 
 	if (fields.test(SerialNumberColumn))
-		p += std::sprintf(buffer + p, "%10lu\t",
-				c->getCreatedSerialNumber());
+		p += std::sprintf(buffer + p, "%10lu\t", c->getCreatedSerialNumber());
 	if (fields.test(CreatedIdColumn))
 		p += std::sprintf(buffer + p, "%10i\t", c->created.getId());
 	if (fields.test(CreatedEnergyColumn))
-		p += std::sprintf(buffer + p, "%8.5E\t",
-				c->created.getEnergy() / energyScale);
+		p += std::sprintf(buffer + p, "%8.5E\t", c->created.getEnergy() / energyScale);
 	if (fields.test(CreatedPositionColumn)) {
 		if (oneDimensional) {
-			p += std::sprintf(buffer + p, "%8.5E\t",
-					c->created.getPosition().x / lengthScale);
+			p += std::sprintf(buffer + p, "%8.5E\t", c->created.getPosition().x / lengthScale);
 		} else {
 			const Vector3d pos = c->created.getPosition() / lengthScale;
-			p += std::sprintf(buffer + p, "%8.5E\t%8.5E\t%8.5E\t", pos.x, pos.y,
-					pos.z);
+			p += std::sprintf(buffer + p, "%8.5E\t%8.5E\t%8.5E\t", pos.x, pos.y, pos.z);
 		}
 	}
 	if (fields.test(CreatedDirectionColumn)) {
 		if (not oneDimensional) {
 			const Vector3d pos = c->created.getDirection();
-			p += std::sprintf(buffer + p, "%8.5E\t%8.5E\t%8.5E\t", pos.x, pos.y,
-					pos.z);
+			p += std::sprintf(buffer + p, "%8.5E\t%8.5E\t%8.5E\t", pos.x, pos.y, pos.z);
 		}
 	}
 	if (fields.test(WeightColumn)) {
@@ -257,22 +209,21 @@ void TextOutput::process(Candidate *c) const {
 		p += std::sprintf(buffer + p, "%s\t", c->getTagOrigin().c_str());
 	}
 
-	for(std::vector<Output::Property>::const_iterator iter = properties.begin();
-			iter != properties.end(); ++iter) {
-		  Variant v;
-			if (c->hasProperty((*iter).name)) {
-				v = c->getProperty((*iter).name);
-			} else {
-				v = (*iter).defaultValue;
-			}
-			p += std::sprintf(buffer + p, "%s", v.toString("\t").c_str());
-			p += std::sprintf(buffer + p, "\t");
+	for(std::vector<Output::Property>::const_iterator iter = properties.begin(); iter != properties.end(); ++iter) {
+		Variant v;
+		if (c->hasProperty((*iter).name)) {
+			v = c->getProperty((*iter).name);
+		} else {
+			v = (*iter).defaultValue;
+		}
+		p += std::sprintf(buffer + p, "%s", v.toString("\t").c_str());
+		p += std::sprintf(buffer + p, "\t");
 	}
 	buffer[p - 1] = '\n';
 
 	std::locale::global(old_locale);
 
-#pragma omp critical(FileOutput)
+	#pragma omp critical(FileOutput) 
 	{
 		if (count == 0)
 			printHeader();
@@ -285,22 +236,22 @@ void TextOutput::process(Candidate *c) const {
 void TextOutput::load(const std::string &filename, ParticleCollector *collector){
 
 	std::string line;
-	std::istream *in;
+	std::istream* in;
 	std::ifstream infile(filename.c_str());
 	
 	double lengthScale = Mpc; // default Mpc
 	double energyScale = EeV; // default EeV
 
-	if (!infile.good())
+	if (! infile.good())
 		throw std::runtime_error("crpropa::TextOutput: could not open file " + filename);
 	in = &infile;
 	
-	if (kiss::ends_with(filename, ".gz")){
-#ifdef CRPROPA_HAVE_ZLIB
-		in = new zstream::igzstream(*in);
-#else
-		throw std::runtime_error("CRPropa was built without Zlib compression!");
-#endif
+	if (kiss::ends_with(filename, ".gz")) {
+		#ifdef CRPROPA_HAVE_ZLIB
+			in = new zstream::igzstream(*in);
+		#else
+			throw std::runtime_error("CRPropa was built without Zlib compression!");
+		#endif
 	}
 
 	while (std::getline(*in, line)) {
@@ -312,7 +263,7 @@ void TextOutput::load(const std::string &filename, ParticleCollector *collector)
 		double val_d; int val_i;
 		double x, y, z;
 		stream >> val_d;
-		c->setTrajectoryLength(val_d*lengthScale); // D
+		c->setTrajectoryLength(val_d * lengthScale); // D
 		stream >> val_d;
 		c->setRedshift(val_d); // z
 		stream >> val_i;
@@ -320,25 +271,25 @@ void TextOutput::load(const std::string &filename, ParticleCollector *collector)
 		stream >> val_i;
 		c->current.setId(val_i); // ID
 		stream >> val_d;
-		c->current.setEnergy(val_d*energyScale); // E
+		c->current.setEnergy(val_d * energyScale); // E
 		stream >> x >> y >> z;
-		c->current.setPosition(Vector3d(x, y, z)*lengthScale); // X, Y, Z
+		c->current.setPosition(Vector3d(x, y, z) * lengthScale); // X, Y, Z
 		stream >> x >> y >> z;
-		c->current.setDirection(Vector3d(x, y, z)*lengthScale); // Px, Py, Pz
+		c->current.setDirection(Vector3d(x, y, z) * lengthScale); // Px, Py, Pz
 		stream >> val_i; // SN0 (TODO: Reconstruct the parent-child relationship)
 		stream >> val_i;
 		c->source.setId(val_i); // ID0
 		stream >> val_d;
-		c->source.setEnergy(val_d*energyScale);	// E0
+		c->source.setEnergy(val_d * energyScale);	// E0
 		stream >> x >> y >> z;
-		c->source.setPosition(Vector3d(x, y, z)*lengthScale); // X0, Y0, Z0
+		c->source.setPosition(Vector3d(x, y, z) * lengthScale); // X0, Y0, Z0
 		stream >> x >> y >> z;
-		c->source.setDirection(Vector3d(x, y, z)*lengthScale); // P0x, P0y, P0z
+		c->source.setDirection(Vector3d(x, y, z) * lengthScale); // P0x, P0y, P0z
 		stream >> val_i; // SN1
 		stream >> val_i;
 		c->created.setId(val_i); // ID1
 		stream >> val_d;
-		c->created.setEnergy(val_d*energyScale); // E1
+		c->created.setEnergy(val_d * energyScale); // E1
 		stream >> x >> y >> z;
 		c->created.setPosition(Vector3d(x, y, z)*lengthScale); // X1, Y1, Z1
 		stream >> x >> y >> z;
@@ -356,14 +307,14 @@ std::string TextOutput::getDescription() const {
 }
 
 void TextOutput::close() {
-#ifdef CRPROPA_HAVE_ZLIB
-	zstream::ogzstream *zs = dynamic_cast<zstream::ogzstream *>(out);
-	if (zs) {
-		zs->close();
-		delete out;
-		out = 0;
-	}
-#endif
+	#ifdef CRPROPA_HAVE_ZLIB
+		zstream::ogzstream* zs = dynamic_cast<zstream::ogzstream*>(out);
+		if (zs) {
+			zs->close();
+			delete out;
+			out = 0;
+		}
+	#endif
 	outfile.flush();
 }
 
@@ -372,15 +323,15 @@ TextOutput::~TextOutput() {
 }
 
 void TextOutput::gzip() {
-#ifdef CRPROPA_HAVE_ZLIB
-	out = new zstream::ogzstream(*out);
-#else
-	throw std::runtime_error("CRPropa was built without Zlib compression!");
-#endif
+	#ifdef CRPROPA_HAVE_ZLIB
+		out = new zstream::ogzstream(*out);
+	#else
+		throw std::runtime_error("CRPropa was built without Zlib compression!");
+	#endif
 }
 
 void TextOutput::dumpIndexList(std::vector<int> indices) {
-#pragma omp critical(FileOutput)
+	#pragma omp critical(FileOutput) 
 	{
 		std::stringstream ss;
 		ss << "#" << "\t";
