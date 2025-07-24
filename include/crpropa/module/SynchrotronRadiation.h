044a1903cf42ed05b273bec1a052cbcf489b98e9
Rafael <rafaelab@users.noreply.github.com>
2025-07-24 16:37:52 +0200

    first implementation of synchrotron sampling

--- include/crpropa/module/SynchrotronRadiation.h
+++ include/crpropa/module/SynchrotronRadiation.h
@@ -1,8 +1,17 @@
 #ifndef CRPROPA_SYNCHROTRONRADIATION_H
 #define CRPROPA_SYNCHROTRONRADIATION_H
 
+
+#include <fstream>
+#include <limits>
+#include <stdexcept>
+
 #include "crpropa/Module.h"
+#include "crpropa/Random.h"
+#include "crpropa/Sampler.h"
+#include "crpropa/Units.h"
 #include "crpropa/magneticField/MagneticField.h"
+
 
 namespace crpropa {
 /**
@@ -11,91 +20,97 @@ namespace crpropa {
  */
 
 /**
- @class SynchrotronRadiation
- @brief Synchrotron radiation of charged particles in magnetic fields.
-
- This module simulates the continuous energy loss of charged particles in magnetic fields, c.f. Jackson.
- The magnetic field is specified either by a MagneticField or by a RMS field strength value.
- The module limits the next step size to ensure a fractional energy loss dE/E < limit (default = 0.1).
- Optionally, synchrotron photons above a threshold (default E > 10^6 eV) are created as secondary particles.
- Note that the large number of secondary photons per propagation can cause memory problems.
- To mitigate this, use thinning. However, this still does not solve the problem completely.
- For this reason, a break-condition stops tracking secondary photons and reweights the current ones. 
+ * @class SynchrotronRadiation
+ * @brief Synchrotron radiation of charged particles in magnetic fields.
+ * 
+ * This module simulates the continuous energy loss of charged particles in magnetic fields, c.f. Jackson.
+ * The magnetic field is specified either by a MagneticField or by a RMS field strength value.
+ * The module limits the next step size to ensure a fractional energy loss dE/E < limit (default = 0.1).
+ * Optionally, synchrotron photons above a threshold (default E > 10^6 eV) are created as secondary particles.
+ * Note that the large number of secondary photons per propagation can cause memory problems.
+ * To mitigate this, use thinning. However, this still does not solve the problem completely.
+ * For this reason, a break-condition stops tracking secondary photons and reweights the current ones. 
  */
 class SynchrotronRadiation: public Module {
-private:
-	ref_ptr<MagneticField> field; ///< MagneticField instance
-	double Brms; ///< Brms value in case no MagneticField is specified
-	double limit; ///< fraction of energy loss length to limit the next step
-	double thinning; ///< thinning parameter for weighted-sampling (maximum 1, minimum 0)
-	bool havePhotons; ///< flag for production of secondary photons
-	int maximumSamples; ///< maximum number of samples of synchrotron photons (break condition; defaults to 100; 0 or <0 means no sampling)
-	double secondaryThreshold; ///< threshold energy for secondary photons
-	std::vector<double> tabx; ///< tabulated fraction E_photon/E_critical from 10^-6 to 10^2 in 801 log-spaced steps
-	std::vector<double> tabCDF; ///< tabulated CDF of synchrotron spectrum
-	std::string interactionTag = "SYN";
+	protected:
+		ref_ptr<MagneticField> field; ///< MagneticField instance
+		double Brms; ///< Brms value in case no MagneticField is specified
+		double limit; ///< fraction of energy loss length to limit the next step
+		bool havePhotons; ///< flag for production of secondary photons
+		double secondaryThreshold; ///< threshold energy for secondary photons
+		std::vector<double> tabx; ///< tabulated fraction E_photon/E_critical from 10^-6 to 10^2 in 801 log-spaced steps
+		std::vector<double> tabCDF; ///< tabulated CDF of synchrotron spectrum
+		std::string interactionTag;
+		ref_ptr<SamplerEvents> samplerEvents;
+		ref_ptr<SamplerDistribution> samplerDistribution;
+		int maximumSamples; ///< maximum number of samples of synchrotron photons (break condition; 0 or <0 means no sampling)
 
-public:
-	/** Constructor
-	 @param field			magnetic field object
-	 @param havePhotons		if true, add secondary photons as candidates
-	 @param thinning		weighted sampling of secondaries (0: all particles are tracked; 1: maximum thinning)
-	 @param nSamples		number of synchrotron photons to be sampled and added as candidates
-	 @param limit			step size limit as fraction of mean free path
-	 */
-	SynchrotronRadiation(ref_ptr<MagneticField> field, bool havePhotons = false, double thinning = 0, int nSamples = 0, double limit = 0.1);
-	/** Constructor
-	 @param Brms			RMS of the magnetic field (if magnetic-field object not provided)
-	 @param havePhotons		if true, add secondary photons as candidates
-	 @param thinning		weighted sampling of secondaries (0: all particles are tracked; 1: maximum thinning)
-	 @param nSamples		number of synchrotron photons to be sampled and added as candidates
-	 @param limit			step size limit as fraction of mean free path
-	 */
-	SynchrotronRadiation(double Brms = 0, bool havePhotons = false, double thinning = 0, int nSamples = 0, double limit = 0.1);
-	
-	// set the target photon field
-	void setField(ref_ptr<MagneticField> field);
+	public:
+		/** Constructor
+		 * @param field			magnetic field object
+		 * @param havePhotons	if true, add secondary photons as candidates
+		 * @param samplingEvts 	sampler for events (acceptance/rejection of individual secondaries)
+		 * @param samplingDist 	sampler for randomly drawing a few secondaries from the distribution
+		 * @param nSamples		number of synchrotron photons to be sampled and added as candidates
+		 * @param limit			step size limit as fraction of mean free path
+		 */
+		SynchrotronRadiation(ref_ptr<MagneticField> field, bool havePhotons = false, ref_ptr<SamplerEvents> sampling = ref_ptr<SamplerEvents>(new SamplerEventsNull()), ref_ptr<SamplerDistribution> samplingDist = nullptr, int maximumSamples = 0, double limit = 0.1);
+		/** Constructor
+		 * @param Brms			RMS of the magnetic field (if magnetic-field object not provided)
+		 * @param havePhotons	if true, add secondary photons as candidates
+		 * @param thinning		weighted sampling of secondaries (0: all particles are tracked; 1: maximum thinning)
+		 * @param nSamples		number of synchrotron photons to be sampled and added as candidates
+		 * @param limit			step size limit as fraction of mean free path
+		 */
+		SynchrotronRadiation(double Brms = 0, bool havePhotons = false, ref_ptr<SamplerEvents> sampling = ref_ptr<SamplerEvents>(new SamplerEventsNull()), ref_ptr<SamplerDistribution> samplingDist = nullptr, int nSamples = 0, double limit = 0.1);
+		
+		// set the target photon field
+		void setField(ref_ptr<MagneticField> field);
 
-	// set the root-mean square (rms) value of the magnetic field (no 3d field is used)
-	void setBrms(double Brms);	
+		// set the root-mean square (rms) value of the magnetic field (no 3d field is used)
+		void setBrms(double Brms);	
 
-	// decide if secondary photons are added to the simulation
-	void setHavePhotons(bool havePhotons);
+		// decide if secondary photons are added to the simulation
+		void setHavePhotons(bool havePhotons);
 
-	/** Apply thinning with a given thinning factor
-	 * @param thinning factor of thinning (0: no thinning, 1: maximum thinning)
-	 */
-	void setThinning(double thinning);
+		/** Limit the propagation step to a fraction of the mean free path
+		 * @param limit fraction of the mean free path
+		 */
+		void setLimit(double limit);
 
-	/** Limit the propagation step to a fraction of the mean free path
-	 * @param limit fraction of the mean free path
-	 */
-	void setLimit(double limit);
+		// set the sampler for events (acceptance/rejection of individual secondaries)
+		void setSamplerEvents(ref_ptr<SamplerEvents> sampler);
 
-	/** Set the maximum number of synchrotron photons that will be allowed to be added as candidates. 
-	 This choice depends on the problem at hand. It must be such that all relevant physics is captured with the sample. Weights are added accordingly and the column 'weight' must be added to output.
-	 @param nmax	maximum number of synchrotron photons to be sampled
-	 */
-	void setMaximumSamples(int nmax);
-	/** Synchrotron photons above the secondary energy threshold are added as candidates.
-	 This may lead to a quick increase in memory.
-	 @param threshold	energy threshold above which photons will be added [in Joules]
-	 */
-	void setSecondaryThreshold(double threshold);	
-	void setInteractionTag(std::string tag);
-	ref_ptr<MagneticField> getField();
+		// set the sampler for randomly drawing a few secondaries from the distribution (stored in a histogram)
+		void setSamplerDistribution(ref_ptr<SamplerDistribution>sampler);
 
-	double getBrms();
-	bool getHavePhotons();
-	double getThinning();
-	double getLimit();
-	int getMaximumSamples();
-	double getSecondaryThreshold() const;
-	std::string getInteractionTag() const;
+		/** Set the maximum number of synchrotron photons that will be allowed to be added as candidates.
+		 * This choice depends on the problem at hand. It must be such that all relevant physics is captured with the sample. Weights are added accordingly and the column 'weight' must be added to output.
+		 * @param nMax	maximum number of synchrotron photons to be sampled
+		 */
+		void setMaximumSamples(int nMax);
 
-	void initSpectrum();
-	void process(Candidate *candidate) const;
-	std::string getDescription() const;
+		/** Synchrotron photons above the secondary energy threshold are added as candidates.
+		 * This may lead to a quick increase in memory.
+		 * @param threshold	energy threshold above which photons will be added [in Joules]
+		 */
+		void setSecondaryThreshold(double threshold);	
+		void setInteractionTag(std::string tag);
+
+		double getBrms() const;
+		ref_ptr<MagneticField> getField() const;
+		bool getHavePhotons() const;
+		double getLimit() const;
+		int getMaximumSamples() const;
+		ref_ptr<SamplerEvents> getSamplerEvents() const;
+		ref_ptr<SamplerDistribution> getSamplerDistribution() const;
+		double getSecondaryThreshold() const;
+		std::string getInteractionTag() const;
+
+		void initSpectrum();
+		void process(Candidate* candidate) const;
+
+		std::string getDescription() const;
 };
 /** @}*/
 
--- src/module/SynchrotronRadiation.cpp
+++ src/module/SynchrotronRadiation.cpp
@@ -1,14 +1,10 @@
 #include "crpropa/module/SynchrotronRadiation.h"
-#include "crpropa/Units.h"
-#include "crpropa/Random.h"
 
-#include <fstream>
-#include <limits>
-#include <stdexcept>
 
 namespace crpropa {
 
-SynchrotronRadiation::SynchrotronRadiation(ref_ptr<MagneticField> field, bool havePhotons, double thinning, int nSamples, double limit) {
+SynchrotronRadiation::SynchrotronRadiation(ref_ptr<MagneticField> field, bool havePhotons, ref_ptr<SamplerEvents> samplingEvts, ref_ptr<SamplerDistribution> samplingDist, int nSamples, double limit) {
+	setInteractionTag("SYN");
 	setField(field);
 	setBrms(0);
 	initSpectrum();
@@ -16,65 +12,78 @@ SynchrotronRadiation::SynchrotronRadiation(ref_ptr<MagneticField> field, bool ha
 	setLimit(limit);
 	setSecondaryThreshold(1e6 * eV);
 	setMaximumSamples(nSamples);
-	setThinning(thinning);
+	setSamplerEvents(samplingEvts);
+	setSamplerDistribution(samplingDist);
 }
 
-SynchrotronRadiation::SynchrotronRadiation(double Brms, bool havePhotons, double thinning, int nSamples, double limit) {
+SynchrotronRadiation::SynchrotronRadiation(double Brms, bool havePhotons, ref_ptr<SamplerEvents> samplingEvts, ref_ptr<SamplerDistribution> samplingDist, int nSamples, double limit) {
+	setInteractionTag("SYN");
 	setBrms(Brms);
 	initSpectrum();
 	setHavePhotons(havePhotons);
 	setLimit(limit);
 	setSecondaryThreshold(1e6 * eV);
 	setMaximumSamples(nSamples);
-	setThinning(thinning);
+	setSamplerEvents(samplingEvts);
+	setSamplerDistribution(samplingDist);
 }
 
 void SynchrotronRadiation::setField(ref_ptr<MagneticField> f) {
-	this->field = f;
+	field = f;
 }
 
-ref_ptr<MagneticField> SynchrotronRadiation::getField() {
+ref_ptr<MagneticField> SynchrotronRadiation::getField() const {
 	return field;
 }
 
-void SynchrotronRadiation::setBrms(double Brms) {
-	this->Brms = Brms;
+void SynchrotronRadiation::setBrms(double B) {
+	Brms = B;
 }
 
-double SynchrotronRadiation::getBrms() {
+double SynchrotronRadiation::getBrms() const {
 	return Brms;
 }
 
-void SynchrotronRadiation::setHavePhotons(bool havePhotons) {
-	this->havePhotons = havePhotons;
+void SynchrotronRadiation::setHavePhotons(bool photons) {
+	havePhotons = photons;
 }
 
-bool SynchrotronRadiation::getHavePhotons() {
+bool SynchrotronRadiation::getHavePhotons() const {
 	return havePhotons;
 }
 
-void SynchrotronRadiation::setThinning(double thinning) {
-	this->thinning = thinning;
+
+void SynchrotronRadiation::setLimit(double lim) {
+	limit = lim;
 }
 
-double SynchrotronRadiation::getThinning() {
-	return thinning;
-}
-
-void SynchrotronRadiation::setLimit(double limit) {
-	this->limit = limit;
-}
-
-double SynchrotronRadiation::getLimit() {
+double SynchrotronRadiation::getLimit() const {
 	return limit;
 }
 
-void SynchrotronRadiation::setMaximumSamples(int nmax) {
-	maximumSamples = nmax;
+void SynchrotronRadiation::setMaximumSamples(int nSamples) {
+	maximumSamples = nSamples;
 }
 
-int SynchrotronRadiation::getMaximumSamples() {
+int SynchrotronRadiation::getMaximumSamples() const {
 	return maximumSamples;
+}
+
+
+void SynchrotronRadiation::setSamplerEvents(ref_ptr<SamplerEvents> s) {
+	samplerEvents = s;
+}
+
+ref_ptr<SamplerEvents> SynchrotronRadiation::getSamplerEvents() const {
+	return samplerEvents;
+}
+
+void SynchrotronRadiation::setSamplerDistribution(ref_ptr<SamplerDistribution> s) {
+	samplerDistribution = s;
+}
+
+ref_ptr<SamplerDistribution> SynchrotronRadiation::getSamplerDistribution() const {
+	return samplerDistribution;
 }
 
 void SynchrotronRadiation::setSecondaryThreshold(double threshold) {
@@ -85,11 +94,20 @@ double SynchrotronRadiation::getSecondaryThreshold() const {
 	return secondaryThreshold;
 }
 
+void SynchrotronRadiation::setInteractionTag(std::string tag) {
+	interactionTag = tag;
+}
+
+std::string SynchrotronRadiation::getInteractionTag() const {
+	return interactionTag;
+}
+
+
 void SynchrotronRadiation::initSpectrum() {
 	std::string filename = getDataPath("Synchrotron/spectrum.txt");
 	std::ifstream infile(filename.c_str());
 
-	if (!infile.good())
+	if (! infile.good())
 		throw std::runtime_error("SynchrotronRadiation: could not open file " + filename);
 
 	// clear previously loaded interaction rates
@@ -110,7 +128,7 @@ void SynchrotronRadiation::initSpectrum() {
 	infile.close();
 }
 
-void SynchrotronRadiation::process(Candidate *candidate) const {
+void SynchrotronRadiation::process(Candidate* candidate) const {
 	double charge = fabs(candidate->current.getCharge());
 	if (charge == 0)
 		return; // only charged particles
@@ -139,68 +157,74 @@ void SynchrotronRadiation::process(Candidate *candidate) const {
 	candidate->limitNextStep(limit * E / dEdx);
 
 	// optionally add secondary photons
-	if (not(havePhotons))
-		return;
+	if (havePhotons) {
+		// check if photons with energies > 14 * Ecrit are possible
+		double Ecrit = 3. / 4 * h_planck / M_PI * c_light * pow(lf, 3) / Rg;
+		if (14 * Ecrit < secondaryThreshold)
+			return;
 
-	// check if photons with energies > 14 * Ecrit are possible
-	double Ecrit = 3. / 4 * h_planck / M_PI * c_light * pow(lf, 3) / Rg;
-	if (14 * Ecrit < secondaryThreshold)
-		return;
+		// draw photons up to the total energy loss
+		// if maximumSamples is reached before that, compensate the total energy afterwards
+		Random& random = Random::instance();
+		double dE0 = dE;
+		std::vector<double> energies;
+		int counter = 0;
+		while (dE > 0) {
+			// draw random value between 0 and maximum of corresponding cdf
+			// choose bin of s where cdf(x) = cdf_rand -> x_rand
+			size_t i = random.randBin(tabCDF); // draw random bin (upper bin boundary returned)
+			double binWidth = (tabx[i] - tabx[i - 1]);
+			double x = tabx[i - 1] + random.rand() * binWidth; // draw random x uniformly distributed in bin
+			double Ephoton = x * Ecrit;
 
-	// draw photons up to the total energy loss
-	// if maximumSamples is reached before that, compensate the total energy afterwards
-	Random &random = Random::instance();
-	double dE0 = dE;
-	std::vector<double> energies;
-	int counter = 0;
-	while (dE > 0) {
-		// draw random value between 0 and maximum of corresponding cdf
-		// choose bin of s where cdf(x) = cdf_rand -> x_rand
-		size_t i = random.randBin(tabCDF); // draw random bin (upper bin boundary returned)
-		double binWidth = (tabx[i] - tabx[i-1]);
-		double x = tabx[i-1] + random.rand() * binWidth; // draw random x uniformly distributed in bin
-		double Ephoton = x * Ecrit;
+			// if the remaining energy is not sufficient check for random accepting
+			if (Ephoton > dE) {
+				if (random.rand() > (dE / Ephoton))
+					break; // not accepted
+			}
 
-		// if the remaining energy is not sufficient check for random accepting
-		if (Ephoton > dE) {
-			if (random.rand() > (dE / Ephoton))
-				break; // not accepted
+			if (samplerDistribution == nullptr or maximumSamples <= 0) {
+				double Es = Ephoton / 2;
+				double w = samplerEvents->computeWeight(22, Es, Es / E, counter);
+				Vector3d pos = random.randomInterpolatedPosition(candidate->previous.getPosition(), candidate->current.getPosition());
+				if (w > 0)
+					candidate->addSecondary(-11, Es, pos, w, interactionTag);
+			} else {
+				samplerDistribution->push(Ephoton / 2.);
+			}
+
+			if (maximumSamples > 0 and counter > maximumSamples)
+				break;
+
+			// energy loss
+			dE -= Ephoton;
+
+			// counter for sampling break condition;
+			counter++;
 		}
 
-		// only activate the "per-step" sampling if maximumSamples is explicitly set.
-		if (maximumSamples > 0) {
-			if (counter >= maximumSamples) 
-				break;			
+		if (samplerDistribution != nullptr and maximumSamples > 0) {
+			samplerDistribution->transformToPDF();
+			samplerDistribution->transformToCDF();
+			std::vector<double> sampledPhotons = samplerDistribution->getSample(maximumSamples);
+			double dEs = std::accumulate(sampledPhotons.begin(), sampledPhotons.end(), decltype(sampledPhotons)::value_type(0)) * 2;
+			if (samplerDistribution->getSize() > 0) {
+				double w = dE0 / dEs;
+				for (size_t i = 0; i < sampledPhotons.size(); i++) {
+					double Es = sampledPhotons[i];
+
+					// this was not thoroughly tested yet, although it should work
+					// w *= samplerEvents->computeWeight(-11, Es, Es / E0, i); 
+					if (w > 0) {
+						Vector3d pos = random.randomInterpolatedPosition(candidate->previous.getPosition(), candidate->current.getPosition());
+						candidate->addSecondary(-11, Es, pos, w, interactionTag);
+					}
+				}
+				samplerDistribution->clear();
+			}
 		}
-
-		// store energies in array
-		energies.push_back(Ephoton);
-
-		// energy loss
-		dE -= Ephoton;
-
-		// counter for sampling break condition;
-		counter++;
 	}
-
-	// while loop before gave total energy which is just a fraction of the required
-	double w1 = 1;
-	if (maximumSamples > 0 && dE > 0)
-		w1 = 1. / (1. - dE / dE0); 
-
-	// loop over sampled photons and attribute weights accordingly
-	for (int i = 0; i < energies.size(); i++) {
-		double Ephoton = energies[i];
-		double f = Ephoton / (E - dE0);
-		double w = w1 / pow(f, thinning);
-
-		// thinning procedure: accepts only a few random secondaries
-		if (random.rand() < pow(f, thinning)) {
-			Vector3d pos = random.randomInterpolatedPosition(candidate->previous.getPosition(), candidate->current.getPosition());
-			if (Ephoton > secondaryThreshold) // create only photons with energies above threshold
-				candidate->addSecondary(22, Ephoton, pos, w, interactionTag);
-		}
-	}
+	
 }
 
 std::string SynchrotronRadiation::getDescription() const {
@@ -216,17 +240,8 @@ std::string SynchrotronRadiation::getDescription() const {
 		s << ", no synchrotron photons";
 	if (maximumSamples > 0)
 		s << "maximum number of photon samples: " << maximumSamples;
-	if (thinning > 0)
-		s << "thinning parameter: " << thinning; 
 	return s.str();
 }
 
-void SynchrotronRadiation::setInteractionTag(std::string tag) {
-	interactionTag = tag;
-}
-
-std::string SynchrotronRadiation::getInteractionTag() const {
-	return interactionTag;
-}
 
 } // namespace crpropa
