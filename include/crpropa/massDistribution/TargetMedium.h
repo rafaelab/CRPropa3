f43ea3c64751624fb84ffea1d01688ab70da4283
Rafael <rafaelab@users.noreply.github.com>
2025-07-25 23:05:56 +0200

    implementation of target medium, including aliases

--- include/crpropa/massDistribution/TargetMedium.h
+++ include/crpropa/massDistribution/TargetMedium.h
@@ -1,10 +1,12 @@
-#ifndef CRPROPA_MEDIUMCOMPOSION_H
-#define CRPROPA_MEDIUMCOMPOSION_H
+#ifndef CRPROPA_TARGETMEDIUM_H
+#define CRPROPA_TARGETMEDIUM_H
 
 
 #include <numeric>
 #include <sstream>
 #include <stdexcept>
+#include <string_view>
+#include <utility> 
 #include <vector>
 
 #include "crpropa/ParticleID.h"
@@ -14,165 +16,36 @@
 #include "kiss/logger.h"
 
 
-
 namespace crpropa {
 
+
 /**
- * @class MediumComposition
- * @brief Abstract base class for medium compositions.
- * 
- * This class serves as an interface for different types of medium compositions, such as elementary particles, atomic nuclei, and molecular compositions.
+ * @brief Class defining a given target medium
+ *
+ * Provides virtual methods for name and weight, allowing runtime polymorphism.
  */
-class MediumComposition: public Referenced {
+class TargetMedium {
 	protected:
-		std::string label;
+		std::string name = "unknown target"; ///< Default member initializer
+		double weight = 1.0; ///< Default member initializer
 
 	public:
-		virtual ~MediumComposition() = default;
-		virtual bool isAdmixed() const = 0;
-		virtual bool isMolecular() const = 0;
-		virtual bool isIonized() const = 0;
-		virtual bool isNeutral() const = 0;
-		bool isCharged() const {
-			return ! isNeutral();
-		};
-		virtual double getCompositionWeight() const = 0;
-		virtual std::string getDescription() const = 0;
-		virtual unsigned int numberOfComponents() const {
-			return 1;
-		};
-		void setLabel(const std::string& label) {
-			this->label = label;
-		};
-		std::string getLabel() const {
-			return label;
-		}
+		explicit TargetMedium(double weight = 1) noexcept;
+		TargetMedium(std::string_view name, double weight = 1) noexcept; 
+		void setName(std::string n) noexcept;
+		void setWeight(double w) noexcept;
+		[[nodiscard]] std::string getName() const noexcept;
+		[[nodiscard]] double getWeight() const noexcept;
+		[[nodiscard]] std::string getDescription() const noexcept;
 };
 
 
-/**
- * @class MediumCompositionElementary
- * @brief A class representing an elementary medium composition.
- * 
- * This class allows for the representation of fundamental particles, such as electrons, with their particle ID.
- */
-class MediumCompositionElementary : public MediumComposition {
-	protected:
-		int particleId;
+static inline const TargetMedium TargetHI = TargetMedium("HI", 1.0);
+static inline const TargetMedium TargetHII = TargetMedium("HII", 1.0);
+static inline const TargetMedium TargetH2 = TargetMedium("H2", 2.0);
+static inline const TargetMedium TargetHe = TargetMedium("He", 4.0);
 
-	public:
-		MediumCompositionElementary(std::string label = "elementary");
-		MediumCompositionElementary(int id, std::string label = "elementary");
-		void setParticleId(int id);
-		int getParticleId() const;
-		bool isAdmixed() const override;
-		bool isIonized() const override;
-		bool isNeutral() const override;
-		bool isMolecular() const override;
-		unsigned int getNumberOfNucleons() const;
-		double getCompositionWeight() const override;
-		std::string getDescription() const override;
-};
-
-
-/**
- * @class MediumCompositionAtomic
- * @brief A class representing an atomic medium composition.
- * 
- * This class allows for the representation of atomic nuclei, including their number of electrons.
- */
-class MediumCompositionAtomic : public MediumComposition {
-	protected:
-		int atomId;
-		int nElectrons;
-
-	public:
-		MediumCompositionAtomic(std::string label = "atomic");
-		MediumCompositionAtomic(int id, int nElectrons = 0, std::string label = "atomic");
-		void setAtomId(int id);
-		void setNumberOfElectrons(int n);
-		int getAtomId() const;
-		int getNumberOfElectrons() const;
-		bool isAdmixed() const override;
-		bool isIonized() const override;
-		bool isNeutral() const override;
-		bool isMolecular() const override;
-		unsigned int getNumberOfNucleons() const;
-		double getCompositionWeight() const override;
-		std::string getDescription() const override;
-};
-
-
-// class MediumCompositionHI : public MediumCompositionAtomic {
-// 	public:
-// 		MediumCompositionHI() {
-// 			setNucleusId(1000010010); // default to hydrogen
-// 			setNumberOfElectrons(1);
-// 		}
-// };
-
-// class MediumCompositionHII : public MediumCompositionAtomic {
-// 	public:
-// 		MediumCompositionHII() {
-// 			setNucleusId(1000010010); // default to hydrogen
-// 			setNumberOfElectrons(1);
-// 		}
-// };
-
-/**
- * @class MediumCompositionMolecular
- * @brief A class representing a molecular medium composition.
- * 
- * This class allows for the representation of molecular compositions, such as H2, by storing a list of nuclei IDs and the number of electrons.
- */
-class MediumCompositionMolecular : public MediumComposition {
-	protected:
-		std::vector<int> atomIds;
-		int nElectrons;
-
-	public:
-		MediumCompositionMolecular(std::string label = "molecular");
-		MediumCompositionMolecular(int nElectrons, std::string label = "molecular");
-		MediumCompositionMolecular(const std::vector<int>& ids, int nElectrons = 0, std::string label = "molecular");
-		void add(int id);
-		void setAtomIds(const std::vector<int>& ids);
-		void setNumberOfElectrons(int n);
-		int getNumberOfElectrons() const;
-		const std::vector<int>& getAtomIds() const;
-		bool isAdmixed() const override;
-		bool isIonized() const override;
-		bool isNeutral() const override;
-		bool isMolecular() const override;
-		unsigned int getNumberOfNucleons() const;
-		double getCompositionWeight() const override;
-		std::string getDescription() const override;
-};
-
-
-/**
- * @class MediumCompositionList
- * @brief A list of MediumComposition objects with associated weights.
- */
-class MediumCompositionList : public MediumComposition {
-	protected:
-		std::vector<ref_ptr<MediumComposition>> compositions;
-		std::vector<double> weights;
-
-	public:
-		MediumCompositionList(std::string label = "list");
-		MediumCompositionList(const std::vector<ref_ptr<MediumComposition>>& comps, const std::vector<double>& weights = {}, std::string label = "list");
-		void add(ref_ptr<MediumComposition> composition, double weight = 1.0);
-		const std::vector<ref_ptr<MediumComposition>>& getCompositions() const;
-		const std::vector<double>& getWeights() const;
-		bool isAdmixed() const override;
-		bool isMolecular() const override;
-		bool isIonized() const override;
-		bool isNeutral() const override;
-		unsigned int numberOfComponents() const override;
-		double getCompositionWeight() const override;
-		std::string getDescription() const override;
-};
 
 } // namespace crpropa
 
-#endif // CRPROPA_MEDIUMCOMPOSITION_H
+#endif // CRPROPA_TARGETMEDIUM_H
--- src/massDistribution/TargetMedium.cpp
+++ src/massDistribution/TargetMedium.cpp
@@ -1,276 +1,37 @@
-#include "crpropa/massDistribution/MediumComposition.h"
+#include "crpropa/massDistribution/TargetMedium.h"
 
 
 namespace crpropa {
 
-
-MediumCompositionElementary::MediumCompositionElementary(std::string label) {
+TargetMedium::TargetMedium(double weight) noexcept 
+	: weight(weight) {
 }
 
-MediumCompositionElementary::MediumCompositionElementary(int id, std::string label) {
-	setParticleId(id);
+TargetMedium::TargetMedium(std::string_view name, double weight) noexcept 
+	: name(name), weight(weight) {
 }
 
-void MediumCompositionElementary::setParticleId(int id) {
-	if (isNucleus(id)) {
-		KISS_LOG_WARNING << "MediumCompositionElementary is more appropriate for more fundamental particles. The Id provided is for a nucleus. Try using MediumCompositionAtomic. I will proceed anyway." << std::endl;
-	}
-	particleId = id;
+void TargetMedium::setName(std::string n) noexcept {
+	name = std::move(n);
 }
 
-int MediumCompositionElementary::getParticleId() const {
-	return particleId;
+void TargetMedium::setWeight(double w) noexcept {
+	weight = w;
 }
 
-bool MediumCompositionElementary::isAdmixed() const {
-	return false;
+[[nodiscard]] std::string TargetMedium::getName() const noexcept {
+	return name;
 }
 
-bool MediumCompositionElementary::isMolecular() const {
-	return false;
-}
-
-bool MediumCompositionElementary::isIonized() const {
-	return false;
-}
-
-bool MediumCompositionElementary::isNeutral() const {
-	return HepPID::charge(particleId) == 0;
-}
-
-unsigned int MediumCompositionElementary::getNumberOfNucleons() const {
-	if (particleId == 2212 or particleId == 2112) { // p or n
-		return 1;
-	}
-	if (isNucleus(particleId)) {
-		return massNumber(particleId);
-	}
-	return 0; // no nucleons
-}
-
-double MediumCompositionElementary::getCompositionWeight() const {
-	return 0;
-}
-
-std::string MediumCompositionElementary::getDescription() const {
-	std::stringstream ss;
-	ss << "MediumCompositionElementary with particle ID: " << particleId << std::endl;
-	return ss.str();
-}
-
-
-///////////////////////////////////////////////////////////////////////////////
-
-MediumCompositionAtomic::MediumCompositionAtomic(std::string label) {
-}
-
-MediumCompositionAtomic::MediumCompositionAtomic(int id, int nElectrons, std::string label) {
-	setAtomId(id);
-	setNumberOfElectrons(nElectrons);
-}
-
-void MediumCompositionAtomic::setAtomId(int id) {
-	if (not isNucleus(id)) {
-		KISS_LOG_WARNING << "MediumCompositionAtomic works for atomic nuclei only. You provided a particle that is not a nucleus." << std::endl;
-		throw std::invalid_argument("MediumCompositionAtomic works for atomic nuclei only. You provided a particle that is not a nucleus.");
-	}
-	atomId = id;
-}
-
-void MediumCompositionAtomic::setNumberOfElectrons(int n) {
-	nElectrons = n;
-}
-
-int MediumCompositionAtomic::getAtomId() const {
-	return atomId;
-}
-
-int MediumCompositionAtomic::getNumberOfElectrons() const {
-	return nElectrons;
-}
-
-bool MediumCompositionAtomic::isAdmixed() const {
-	return false;
-}
-
-bool MediumCompositionAtomic::isMolecular() const {
-	return false;
-}
-
-bool MediumCompositionAtomic::isIonized() const {
-	return  nElectrons != chargeNumber(atomId);
-}
-
-bool MediumCompositionAtomic::isNeutral() const {
-	return nElectrons == chargeNumber(atomId);
-}
-
-unsigned int MediumCompositionAtomic::getNumberOfNucleons() const {
-	if (atomId == 2212 or atomId == 2112) { // p or n
-		return 1;
-	}
-	if (isNucleus(atomId)) {
-		return massNumber(atomId);
-	}
-	return 0; // no nucleons
-}
-
-double MediumCompositionAtomic::getCompositionWeight() const {
-	return 1.0 * getNumberOfNucleons();
-}
-
-std::string MediumCompositionAtomic::getDescription() const {
-	std::stringstream ss;
-	ss << "MediumCompositionAtomic with atom ID: " << atomId << " and number of electrons: " << nElectrons << std::endl;
-	return ss.str();
-}
-
-
-///////////////////////////////////////////////////////////////////////////////
-
-MediumCompositionMolecular::MediumCompositionMolecular(std::string label) {
-}
-
-MediumCompositionMolecular::MediumCompositionMolecular(int nElectrons, std::string label) {
-	atomIds.clear();
-	setNumberOfElectrons(nElectrons);
-}
-
-MediumCompositionMolecular::MediumCompositionMolecular(const std::vector<int>& ids, int nElectrons, std::string label) {
-	for (const auto& id : ids) {
-		add(id);
-	}
-	setNumberOfElectrons(nElectrons);
-}
-
-void MediumCompositionMolecular::add(int id) {
-	if (! isNucleus(id)) {
-		KISS_LOG_WARNING << "MediumCompositionMolecular works for atomic nuclei only. You provided a particle that is not a nucleus." << std::endl;
-		throw std::invalid_argument("MediumCompositionMolecular works for atomic nuclei only. You provided a particle that is not a nucleus.");
-	}
-	atomIds.push_back(id);
-}
-
-void MediumCompositionMolecular::setAtomIds(const std::vector<int>& ids) {
-	atomIds = ids;
-}
-
-void MediumCompositionMolecular::setNumberOfElectrons(int n) {
-	nElectrons = n;
-}
-
-int MediumCompositionMolecular::getNumberOfElectrons() const {
-	return nElectrons;
-}
-
-const std::vector<int>& MediumCompositionMolecular::getAtomIds() const {
-	return atomIds;
-}
-
-bool MediumCompositionMolecular::isAdmixed() const {
-	return false;
-}
-
-bool MediumCompositionMolecular::isMolecular() const {
-	return true;
-}
-
-bool MediumCompositionMolecular::isIonized() const {
-	return nElectrons != 0;
-}
-
-bool MediumCompositionMolecular::isNeutral() const {
-	return nElectrons == 0;
-}
-
-unsigned int MediumCompositionMolecular::getNumberOfNucleons() const {
-	int nNucleons = 0;
-	for (const auto& atom : atomIds)
-		nNucleons += massNumber(atom);
-	return nNucleons;
-}
-
-double MediumCompositionMolecular::getCompositionWeight() const {
-	return 1.0 * getNumberOfNucleons();
-}
-
-std::string MediumCompositionMolecular::getDescription() const {
-	std::stringstream ss;
-	ss << "MediumCompositionMolecular with atom IDs: ";
-	for (const auto& id : atomIds) {
-		ss << id << " ";
-	}
-	ss << "and number of electrons: " << nElectrons << std::endl;
-	return ss.str();
-}
-
-
-///////////////////////////////////////////////////////////////////////////////
-
-MediumCompositionList::MediumCompositionList(std::string label) {
-}
-
-MediumCompositionList::MediumCompositionList(const std::vector<ref_ptr<MediumComposition>>& comps, const std::vector<double>& weights, std::string label) {
-	if (comps.size() != weights.size()) {
-		KISS_LOG_WARNING << "MediumCompositionList: Compositions and weights must have the same size." << std::endl;
-		throw std::invalid_argument("MediumCompositionList: Compositions and weights must have the same size.");
-	}
-
-	for (size_t i = 0; i < comps.size(); ++i)
-		add(comps[i], weights[i]);
-}
-
-void MediumCompositionList::add(ref_ptr<MediumComposition> composition, double weight) {
-	compositions.push_back(composition);
-	weights.push_back(weight);
-}
-
-const std::vector<ref_ptr<MediumComposition>>& MediumCompositionList::getCompositions() const {
-	return compositions;
-}
-
-const std::vector<double>& MediumCompositionList::getWeights() const {
-	return weights;
-}
-
-bool MediumCompositionList::isAdmixed() const {
-	return numberOfComponents() > 1;
-}
-
-bool MediumCompositionList::isMolecular() const {
-	return false;
-}
-
-bool MediumCompositionList::isIonized() const {
-	return false;
-}
-
-bool MediumCompositionList::isNeutral() const {
-	return false;
-}
-
-unsigned int MediumCompositionList::numberOfComponents() const {
-	return compositions.size();
-}
-
-double MediumCompositionList::getCompositionWeight() const {
-	double weight = 0.0;
-	double sumWeights = accumulate(weights.begin(), weights.end(), 0.);
-	for (size_t i = 0; i < numberOfComponents(); ++i) {
-		weight += (weights[i] * compositions[i]->getCompositionWeight() / sumWeights);
-	}
-
+[[nodiscard]] double TargetMedium::getWeight() const noexcept {
 	return weight;
 }
 
-std::string MediumCompositionList::getDescription() const {
-	std::stringstream ss;
-	ss << "MediumCompositionList with " << compositions.size() << " components." << std::endl;
-	for (const auto& comp : compositions)
-		ss << comp->getDescription();
-
-	return ss.str();
+[[nodiscard]] std::string TargetMedium::getDescription() const noexcept {
+	std::ostringstream oss;
+	oss << "Target " << name << " (w = " << weight << ")";
+	return oss.str();
 }
 
-
 } // namespace crpropa
+
