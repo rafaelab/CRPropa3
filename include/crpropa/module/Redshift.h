346e8270c14878b289f5ab91b0d33982af7f6692
Rafael <rafaelab@users.noreply.github.com>
2025-07-27 16:59:48 +0200

    compliance with c++20 and improvements

--- include/crpropa/module/Redshift.h
+++ include/crpropa/module/Redshift.h
@@ -1,35 +1,52 @@
-#ifndef CRPROPA_REDSHIFT_H
-#define CRPROPA_REDSHIFT_H
+#pragma once
 
+#include <limits>
+
+#include "crpropa/Cosmology.h"
 #include "crpropa/Module.h"
+#include "crpropa/Units.h"
 
 namespace crpropa {
+
 /**
  * \addtogroup EnergyLosses
  * @{
  */
 
 /**
- @class Redshift
- @brief Updates redshift and applies adiabatic energy loss according to the traveled distance.
+ * @class Redshift
+ * @brief Updates redshift and applies adiabatic energy loss according to the travelled distance.
  */
 class Redshift: public Module {
-public:
-	void process(Candidate *candidate) const;
-	std::string getDescription() const;
+	public:
+		Redshift() = default;
+		Redshift(const Redshift&) = default;
+		Redshift& operator=(const Redshift&) = default;
+		Redshift(Redshift&&) = default;
+		Redshift& operator=(Redshift&&) = default;
+		~Redshift() = default;
+		void process(Candidate* candidate) const override;
+		std::string getDescription() const override;
 };
 
 /**
- @class FutureRedshift
- @brief Updates redshift and applies adiabatic energy loss according to the traveled distance. Extends to negative redshift values to allow for symmetric time windows around z=0.
+ * @class FutureRedshift
+ * @brief Updates redshift and applies adiabatic energy loss according to the traveled distance. 
+ * Extends to negative redshift values to allow for symmetric time windows around z=0.
  */
 class FutureRedshift: public Module {
-public:
-	void process(Candidate *candidate) const;
-	std::string getDescription() const;
+	public:
+		FutureRedshift() = default;
+		FutureRedshift(const FutureRedshift&) = default;
+		FutureRedshift& operator=(const FutureRedshift&) = default;
+		FutureRedshift(FutureRedshift&&) = default;
+		FutureRedshift& operator=(FutureRedshift&&) = default;
+		~FutureRedshift() = default;
+		void process(Candidate* candidate) const override;
+		std::string getDescription() const override;
 };
 
 /** @}*/
+
 } // namespace crpropa
 
-#endif // CRPROPA_REDSHIFT_H
--- src/module/Redshift.cpp
+++ src/module/Redshift.cpp
@@ -1,12 +1,9 @@
 #include "crpropa/module/Redshift.h"
-#include "crpropa/Units.h"
-#include "crpropa/Cosmology.h"
 
-#include <limits>
 
 namespace crpropa {
 
-void Redshift::process(Candidate *c) const {
+void Redshift::process(Candidate* c) const {
 	double z = c->getRedshift();
 
 	// check if z = 0
@@ -29,12 +26,11 @@ void Redshift::process(Candidate *c) const {
 
 std::string Redshift::getDescription() const {
 	std::stringstream s;
-	s << "Redshift: h0 = " << hubbleRate() / 1e5 * Mpc << ", omegaL = "
-			<< omegaL() << ", omegaM = " << omegaM();
+	s << "Redshift: h0 = " << hubbleRate() / 1e5 * Mpc << ", omegaL = " << omegaL() << ", omegaM = " << omegaM();
 	return s.str();
 }
 
-void FutureRedshift::process(Candidate *c) const {
+void FutureRedshift::process(Candidate* c) const {
 	double z = c->getRedshift();
 
 	// check if z = -1
@@ -54,8 +50,7 @@ void FutureRedshift::process(Candidate *c) const {
 
 std::string FutureRedshift::getDescription() const {
 	std::stringstream s;
-	s << "FutureRedshift: h0 = " << hubbleRate() / 1e5 * Mpc << ", omegaL = "
-			<< omegaL() << ", omegaM = " << omegaM();
+	s << "FutureRedshift: h0 = " << hubbleRate() / 1e5 * Mpc << ", omegaL = " << omegaL() << ", omegaM = " << omegaM();
 	return s.str();
 }
 
