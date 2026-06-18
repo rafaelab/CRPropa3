480bff1a4f3d4ceb2befdd03a795370ed436f21b
Jannis Wagner <jwa@tp4.rub.de>
2026-06-18 14:35:52 +0200

    introduce waitForSecondaries parameter

--- include/crpropa/ModuleList.h
+++ include/crpropa/ModuleList.h
@@ -40,8 +40,8 @@ public:
 
 	void run(Candidate* candidate, bool recursive = true, bool secondariesFirst = false); ///< run simulation for a single candidate
 	void run(ref_ptr<Candidate> candidate, bool recursive = true, bool secondariesFirst = false); ///< run simulation for a single candidate
-	void run(const candidate_vector_t *candidates, bool recursive = true, bool secondariesFirst = false); ///< run simulation for a candidate vector
-	void run(SourceInterface* source, size_t count, bool recursive = true, bool secondariesFirst = false); ///< run simulation for a number of candidates from the given source
+	void run(const candidate_vector_t *candidates, bool recursive = true, bool secondariesFirst = false, bool waitForSecondaries); ///< run simulation for a candidate vector
+	void run(SourceInterface* source, size_t count, bool recursive = true, bool secondariesFirst = false, bool waitForSecondaries); ///< run simulation for a number of candidates from the given source
 
 	std::string getDescription() const;
 	void showModules() const;
