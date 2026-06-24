03b66a33db8c0d7d3919dde62f37e2d67a603819
Jan-Niklas Bohnensack <jan-niklas.bohnensack@rub.de>
2026-06-24 17:11:20 +0200

    fixed testMomentumDiffusion.py

--- test/testMomentumDiffusion.py
+++ test/testMomentumDiffusion.py
@@ -61,7 +61,7 @@ class MomentumDiffusion(unittest.TestCase):
         
         bscal = self.momDif.calculateBScalar()
         self.assertEqual(np.sqrt(2*self.Dpp), bscal)
-        ascal = self.momDif.calculateAScalar(p, Dpp)
+        ascal = self.momDif.calculateAScalar(p, self.Dpp)
         self.assertAlmostEqual(2/p*self.Dpp, ascal)
     
     def test_NeutralParticle(self):
